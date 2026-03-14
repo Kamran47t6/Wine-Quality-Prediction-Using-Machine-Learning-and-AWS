f3042ce75a69e3e428cf7832226db5ed3f85ca97
Muhammad Kamran <muhammadkamran5862@gmail.com>
2026-03-14 09:26:15 +0500

    Data Validation Pipeline Updated

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from Wine_Prediction_ML import logger
 from Wine_Prediction_ML.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
+from Wine_Prediction_ML.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+    
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
